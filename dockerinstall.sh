472418c98c40de0111f984ba54ff903f70655ddc
suteetoe <sutee.toe@gmail.com>
2024-01-24 15:50:46 +0700

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,26 +1,16 @@
-# Install Docker on  Ubuntu 14.04
-# Ref https://docs.docker.com/engine/installation/linux/ubuntulinux/
-# Update your APT package index.
+# Add Docker's official GPG key:
 sudo apt-get update
-# Ensure that APT works with the https method, and that CA certificates are installed.
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
-# Add the new GPG key.
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add docker.list 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update your APT package index.
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-# Purge the old repo if it exists.
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-# Verify that APT is pulling from the right repository.
 
-# install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-#set owner
-sudo chmod +x /usr/local/bin/docker-compose
-
-# create network move to runall
-# sudo docker network create sml_service_network
+# install docker 
+sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
